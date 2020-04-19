56ecf025df0d2385a7c03525e315e95040a864d6
ganlei <ganlei@uniontech.com>
2020-04-20 00:36:29 +0800

    oauth2的demo实现（未完成）

--- oauth2-example/oauth2-example3/main.go
+++ oauth2-example/oauth2-example3/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	//"io/ioutil"
 	"log"
 	"net/http"
 
@@ -12,24 +13,24 @@ import (
 const (
 	clientID     = "dc3e12fdda79a72348ae"
 	clientSecret = "0db4b33e1d29bd4e02a5d95cc2f0ad9ff56df742"
-	oauthState   = "normal"
+	oauthState   = "random"
 )
 
 var oauthConfig = &oauth2.Config{
 	ClientID:     clientID,
 	ClientSecret: clientSecret,
 	RedirectURL:  "http://localhost:8080/GithubCallback",
-	Scopes:       []string{"", ""},
+	Scopes:       []string{"user", "gist"},
 	Endpoint: oauth2.Endpoint{
-		AuthURL:  "http://localhost:8888/oauth2/auth",
-		TokenURL: "http://localhost:8888/oauth2/token",
+		AuthURL:  "http://github.com/login/oauth/authorize",
+		TokenURL: "http://github.com/login/oauth/access_token",
 	},
 }
 
 func main() {
 	engine := gin.Default()
 
-	engine.LoadHTMLGlob("templates/**/**")
+	engine.LoadHTMLGlob("templates/**/*")
 	engine.GET("/", handleMain)
 	engine.GET("/login", handleLogin)
 	engine.GET("/GithubCallback", handleGithubCallback)
@@ -48,22 +49,44 @@ func handleLogin(c *gin.Context) {
 }
 
 func handleGithubCallback(c *gin.Context) {
-	state := c.Request.ParseForm.GET("state")
+	//通过请求回跳url地址获取state
+	c.Request.ParseForm()
+	log.Printf("callback parm info======>%#v\n", c.Request.Form)
+	state:=c.Request.URL.Query().Get("state")
 	if state != oauthState {
 		log.Fatalf("invalid oauth state, expected '%s', got '%s'\n", oauthState, state)
 		c.Redirect(http.StatusTemporaryRedirect, "/")
 		return
 	}
-	log.Printf("callback parm info======>%#v\n", c.Request.Form)
-
-	code := c.Request.ParseForm.GET("code")
+	
+    //获取code
+	code := c.Request.Form.Get("code")
 	log.Println("code is ======>", code)
+	//通过code获取token
 	token, err := oauthConfig.Exchange(context.Background(), code)
 	log.Println("token is ========>", token)
 	if err != nil {
-		log.Fatal("code exchange failed,err:%v\n", err)
+		log.Fatalf("code exchange failed,err:%v\n", err)
 		c.Redirect(http.StatusTemporaryRedirect, "/")
 		return
 	}
+	c.JSON(http.StatusOK,gin.H{
+		"code":200,
+		"token":token,
+		"msg":"获取token成功",
+	})
+	// //通过token获取客户信息
+	// userInfoURL:="https://api.github.com/user"
+	// response,err:=http.Get(userInfoURL)
+	// if err!=nil{
+	// 	log.Fatalf("Get token failed,err:%#v\n",err)
+	// 	return
+	// }
+	// defer response.Body.Close()
+	// contents,err:=ioutil.ReadAll(response.Body)
+	// c.JSON(http.StatusOK,gin.H{
+	// 	"code":200,
+	// 	"msg":string(contents),
+	// })
 
 }
